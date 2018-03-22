5068501f86e5f14e5f30bc501b48a5e156c46d46
CallmeMrhu <mrhu666@163.com>
2018-03-22 23:20:59 +0800

    commit

---
+++ removeDuplicates.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Mr.Hu on 22/03/2018.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums);
+
+int main(){
+    vector<int> nums = {1,2,3,4};
+    removeDuplicates(nums);
+
+}
+
+int removeDuplicates(vector<int>& nums) {
+    int size = nums.size();
+    if(size<2)
+        return size;
+    int tmp;
+    for(int i=0;i<size;i++){
+
+
+
+    }
+
+    return size;
+}

6f01e8e1f492c1cbc0d8f9ac20a4825f94a92c38
CallmeMrhu <mrhu666@163.com>
2018-03-24 19:10:46 +0800

    commit

--- Reverse_Integer.cpp
+++ Reverse_Integer.cpp
@@ -4,6 +4,8 @@
 // leetcode reverse integer
 // 将整数进行颠倒，值得注意的是，输入的是int值，但是颠倒后可能会超过32bit，导致overflow
 // 所以在存储结果上，要设置为long long，然后对最终的结果要进行判断，且绝对值是否大于INT_MAX。
+// 注：32bit的int，有符号（signed int）其值的范围在[-2147483647～2147483647]的闭区间，2^32-1
+// 对于无符号（unsigned int）其范围在[0~4294967295]的闭区间 2^32-1
 
 #include <iostream>
 using namespace std;

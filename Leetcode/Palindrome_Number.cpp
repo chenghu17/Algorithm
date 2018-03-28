5479fe170be86745b89057b32cecdd2bac89e66f
CallmeMrhu <mrhu666@163.com>
2018-03-28 11:30:56 +0800

    commit

--- Leetcode/Palindrome_Number.cpp
+++ Leetcode/Palindrome_Number.cpp
@@ -1,7 +1,18 @@
 //
 // Created by Mr.Hu on 28/03/2018.
 //
-
+// leetcode Palindrome Number (回文数)
+// determine whether an integer is a palindrome, without extra space;
+// Note negative integers are not palindromes
+// 解这道回文数判断时，首先要明确负数不属于回文数
+// 第一个解题思路是code中的第一种解法，使用vector来存储判定值，然后用最简单的比较方法进行判断，
+// 当然这样做的话，算法的空间复杂度就达到来O(n)，不符合题目without extra space的要求，
+// 在思考的过程中，题目有提示之前做过的reverse integer，翻转数字，int型翻转后，可能会overflow。
+//
+// 第二个解题思路是差了别人的思路之后再写的，主要是对without extra space的这个要求的理解不到位
+// 第二个方法借助临时变量，考虑到回文数折中后左边剩下的部分，和右边翻转得到的数字相等则为palindrome，
+// 并且这样折中后再翻转就不存在overflow的情况了，这是关键！
+//
 
 #include <iostream>
 #include <vector>
--- tmp.cpp
+++ tmp.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class Book{
+public:
+    int number=0;
+    int title=0;
+};
 int main(){
-    // this place is used to do something unclear
-    for(int i=0;i<=0;i++)
-        continue;
+    // this space is used to do something unclear;
+    //Book book = Book();
+    Book book;
+    cout<<book.number;
     return 0;
 }

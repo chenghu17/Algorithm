fbe7351ed1453f3a62e8bd4f0ce65fa83191467d
CallmeMrhu <mrhu666@163.com>
2018-04-01 19:45:12 +0800

    commit

--- Leetcode/c_121_Buy_Sell_Stock.cpp
+++ Leetcode/c_121_Buy_Sell_Stock.cpp
@@ -1,6 +1,10 @@
 //
 // Created by Mr.Hu on 2018/4/1.
-
+// leetcode 121 Best Time to Buy and Sell Stock
+// 这题的第一感觉就是蛮力法，嵌套循环，然后就出现了一下代码
+// 第一次提交报错误，time limit exceeded，开始以为是出现了死循环，再次检查发现没有
+// 于是坚持提交，通过...但是我看了一下执行时间 462ms，确实有点长哈，蛮力法的复杂度确实太高了。
+// 今天状态不佳，先去写论文去了，明天做122题的时候顺便把这一题的第二种解法想出来并coding。
 
 #include <iostream>
 #include <vector>

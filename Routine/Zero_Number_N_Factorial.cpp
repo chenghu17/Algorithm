6e7af0c30d71c7be185e2ea3fdd4d84b47b37c51
CallmeMrhu <mrhu666@163.com>
2018-04-02 18:59:06 +0800

    commit

--- Routine/Zero_Number_N_Factorial.cpp
+++ Routine/Zero_Number_N_Factorial.cpp
@@ -2,7 +2,9 @@
 // Created by Mr.Hu on 2018/4/2.
 //
 // 给定数字n，计算其n！的结果末尾有多少个0
-
+// 阶乘的结果一般都特别大，所以如果直接算，n稍微大一点就会overflow
+// 所以观察发现，n！结果中尾数0的个数，来源于2x5
+// 所以只需要
 
 #include <iostream>
 using namespace std;

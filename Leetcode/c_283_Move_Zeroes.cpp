22fb16e0cc879d7b710edc835b04712ce85e713b
CallmeMrhu <mrhu666@163.com>
2018-04-12 09:30:59 +0800

    commit

--- Leetcode/c_283_Move_Zeroes.cpp
+++ Leetcode/c_283_Move_Zeroes.cpp
@@ -8,6 +8,14 @@
 // 如果数字不为0，则需要与第一个不为0的数字进行交换，所以解题思路就是当前数字不为0时，找到第一个为0的位置，交换
 // 执行完之后查看了submission detail，发现该方法的运行时间只击败了1.18%的方法，累...
 //
+// 于是观察想出了第二种方案：发现每次交换之后，第一个零元素一定出现在之前为零的下一个位置是，分析：
+// 如果不存在连续的两个零，则每次交换即相邻交换，则零元素交换到下一位，即之前为零的下一个位置；
+// 如果存在连续的两个零，则每次交换，第一个零元素会交换到第二个零元素的后面，那么交换后的第一个零元素即为之前的第二个零元素，即之前为零的下一个位置。
+// 此时的submission detail击败了80%多，而且注意判断条件的顺序会很大程度上影响执行的时间效率
+//
+// 提出两种解法之后，又去网上看了一下别人的代码，贴在下面optional中，代码思想使用两层循环，第一层循环寻找第一个为零的数，
+// 第二层循环寻找第一个非零数，然后进行交换，交换后break，再次进入第一层循环中，继续寻找第一个零元素，值得注意的是，
+// 正如我自己的其他两种方法中的if (i > index)一样，交换的情况我们只需要管在零元素后面的非零元素即可，因为我们要求的是非零元素往前移，而不是往后移动。
 //
 
 #include <iostream>
@@ -37,12 +45,45 @@ public:
             }
         }
     }
+    void moveZeroes_optional(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                for(int j=i+1;j<nums.size();j++){
+                    if(nums[j]!=0){
+                        swap(nums[i],nums[j]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    void moveZeroes_optimal(vector<int> &nums) {
+        int size = nums.size();
+        int index = INT_MAX;
+        bool isZero = true;
+        for (int i = 0; i < size; i++) {
+            if (nums[i] != 0) {
+                // swap
+                if (i > index) {
+                    nums[index] = nums[i];
+                    nums[i] = 0;
+                    index = index + 1;
+                }
+            } else if (nums[i] == 0 && isZero) {
+                index = i;
+                isZero = false;
+            }
+        }
+    }
 };
 
 int main() {
     vector<int> nums = {0, 1, 2, 0, 12, 3};
     Solution solution;
-    solution.moveZeroes(nums);
+//    solution.moveZeroes(nums);
+//    solution.moveZeroes_optional(nums);
+    solution.moveZeroes_optimal(nums);
     auto it = nums.begin();
     for (it; it != nums.end(); it++) {
         cout << *it << " ";

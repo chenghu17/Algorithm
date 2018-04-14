955e3a40652f85e0e8a8c595776cd5affff8cb92
CallmeMrhu <mrhu666@163.com>
2018-04-14 22:01:10 +0800

    commit

--- tmp.cpp
+++ tmp.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+
 using namespace std;
 
-class Book{
+class Book {
 public:
-    int number=0;
-    int title=0;
+    int number = 0;
+    int title = 0;
 };
-int main(){
+
+
+int main() {
+
+    int n;
+//    cin>>n;
+//    int m[n] = {0};
+
     // this space is used to do something unclear;
     //Book book = Book();
-    Book book;
-    cout<<book.number;
+
+//    vector<int> nums ;
+//    nums.push_back(1);
+//    nums.push_back(2);
+//    nums[1] = 0;
+//    cout<<nums[1];
+
+//    set<int> test;
+//    test.insert(1);
+//    cout<<*test.begin();
+
+//    Book book;
+//    cout<<book.number;
+
     return 0;
 }

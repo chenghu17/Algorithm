48a797c692eaabeafaed91a4db07dda8dd624b70
CallmeMrhu <mrhu666@163.com>
2018-06-04 20:04:18 +0800

    commit

---
+++ Leetcode/c_326_Power_of_Three.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Mr.Hu on 2018/6/4.
+//
+// leetcode 326 power of three
+//
+// 这个题目要求判断n是否能否被3的乘方进行表示
+// 和231题方法一模一样，不做过多解释
+//
+
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n != 1) {
+            if (n % 3 == 0) {
+                n /= 3;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    int n = 9;
+    Solution solution;
+    bool isornot = solution.isPowerOfThree(n);
+    cout << isornot << endl;
+    return 0;
+}

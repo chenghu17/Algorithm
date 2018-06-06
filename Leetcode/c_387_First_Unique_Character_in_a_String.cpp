21ea973140c3d39e79609e39b1c80389f66834fe
CallmeMrhu <mrhu666@163.com>
2018-06-06 09:55:02 +0800

    commit

--- Leetcode/c_387_First_Unique_Character_in_a_String.cpp
+++ Leetcode/c_387_First_Unique_Character_in_a_String.cpp
@@ -11,6 +11,12 @@
 // 然后回归到hash table中，对字符串遍历一次，使用map<char,int>记录每个字符出现的次数，
 // 然后再重新遍历一次，每次在hash table中去查询当前字符出现的次数，如果次数等于1，
 // 则将index值赋值为当前的位置，break，结束循环，return index;
+// 代码已经accpted，但是执行时间上只超过来百分之二十多的结果。原因就在于使用map的key来查询value值是一个很慢的过程。
+// 所以想到能否用list来作为存储对象，但是list的index必须为int型，所以遇到了难题...
+//
+// 查了一下别人的solution，发现是可以用list来存储对象的，因为字符串中都是小写字母，而小写字母的个数是有限的，
+// 即26个，所以我们可以将小写字母的ASCII码值映射到0-25，即小写字母的ASCII码值减去a的ASCII码值97即可。
+// 这种方式的执行效率就超过了百分之八十多的结果。
 //
 
 #include <iostream>
@@ -34,12 +40,32 @@ public:
         }
         return index;
     }
+
+    int firstUniqChar_optimal(string s) {
+        int result[26] = {};
+        int c;
+        for (auto m:s) {
+            c = m - 97;
+            result[c]++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            c = s[i] - 97;
+            if (result[c] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 };
 
 int main() {
     string s = "loveyouloveyou";
+    string q = "aabbccd";
     Solution solution;
-    int index = solution.firstUniqChar(s);
-    cout << index << endl;
+    int index_s = solution.firstUniqChar(s);
+    int index_q = solution.firstUniqChar_optimal(q);
+    cout << index_s << endl;
+    cout << index_q << endl;
     return 0;
 }

8e41aed284c2abeadefdf489bf455d612b68dada
CallmeMrhu <mrhu666@163.com>
2018-07-11 00:33:49 +0800

    commit

--- InterviewQ/Go_Up_the_Stairs.cpp
+++ InterviewQ/Go_Up_the_Stairs.cpp
@@ -12,19 +12,21 @@
 //
 // 由于每次都要叠加之前的结果，当台阶数不断增加时，走完楼梯的方法会迅速增加，以至于超过int和long的取值范围。
 // 所以对于之前的结果使用string进行保留，当计算当前结果时，则需要对字符串进行加法，这也就是我们常用的大数加法，
-// 基本思想是三个数字的从个位数开始相加，如果当前位数已经超过了字符串长度，则返回0与其他数字进行相加，
-// 相加的结果考虑进位和当前位的数组，最后将结果进行字符串拼接，直到三个数每位都加完，则返回三个数的结果。
+// 基本思想是三个数字的从个位数开始相加，并考虑之前进位的结果，如果当前位数已经超过了字符串长度，则返回0与其他数字进行相加，
+// 相加的结果考虑进位和当前位的数字，最后将结果进行字符串拼接，直到三个数所有位都加完，则返回三个数的结果。
 //
 
 #include<iostream>
 #include<vector>
+
 using namespace std;
 
 string addStrings(string num1, string num2, string num3) {
     int carry = 0;
     int first, second, third;
     string summary = "";
-    for (int i = (int)num1.length(), j = (int)num2.length(), k = (int)num3.length(); i > 0 || j > 0 || k > 0 || carry > 0;) {
+    for (int i = (int) num1.length(), j = (int) num2.length(), k = (int) num3.length();
+         i > 0 || j > 0 || k > 0 || carry > 0;) {
         first = i > 0 ? num1[--i] - '0' : 0;
         second = j > 0 ? num2[--j] - '0' : 0;
         third = k > 0 ? num3[--k] - '0' : 0;
@@ -41,7 +43,7 @@ string StepUp(int stairs_num) {
     result.emplace_back("2");
     result.emplace_back("4");
     for (int i = 4; i <= stairs_num; ++i) {
-        result.emplace_back(addStrings(result[i-1],result[i-2],result[i-3]));
+        result.emplace_back(addStrings(result[i - 1], result[i - 2], result[i - 3]));
     }
     return result[stairs_num];
 }
@@ -51,7 +53,7 @@ int main() {
     cout << "please enter the number of stairs: ";
     int stairs_num;
     cin >> stairs_num;
-    string count = StepUp(stairs_num);
-    cout << "the sum of methods: " << count;
+    string method_count = StepUp(stairs_num);
+    cout << "the sum of methods: " << method_count;
     return 0;
 }

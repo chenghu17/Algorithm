0919e9f21aa0f35a754d431d6611d1917f1276e7
CallmeMrhu <mrhu666@163.com>
2018-07-26 11:02:59 +0800

    commit

--- Leetcode/c_594_Longest_Harmonious_Subsequence.cpp
+++ Leetcode/c_594_Longest_Harmonious_Subsequence.cpp
@@ -3,6 +3,14 @@
 //
 // leetcode 594 longest harmonious subsequence
 //
+// 题目定义 harmonious subsequence 为最大值与最小值相差1的序列，不要求连续子序列
+//
+// 刚拿到这个题目的思想是对数组进行排序，然后排序数组中相邻变化为1的即满足条件，计算出现的次数，
+// 但是我对于这种方法在边界问题上考虑欠缺，所以转向另外一种：
+// 用map来存储每个数字出现的次数，然后遍历map，如果当前比当前pairwise中的key值大一的键值存在，
+// 即map.count(cur.first+1)为true，则记录两个key出现的次数，并与当前最长序列数进行比较
+// 这里主要是活用map的first,second，以及count()方法
+//
 
 #include <iostream>
 #include <vector>

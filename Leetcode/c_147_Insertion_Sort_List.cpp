d7590e0092141603734e579174f2692de2113dc4
CallmeMrhu <mrhu666@163.com>
2018-08-02 16:43:03 +0800

    commit

--- Leetcode/c_147_Insertion_Sort_List.cpp
+++ Leetcode/c_147_Insertion_Sort_List.cpp
@@ -3,8 +3,14 @@
 //
 // leetcode 147 Insertion sort List
 //
+// 题目要求对给定的链表采用插入排序
 //
-//
+// 链表使用插入排序比较棘手的地方就是前后两个节点的地址都要知道，
+// 所以我对这个题目进行解答的思想是：判断当前节点与第一个节点的大小关系，小于第一个节点，则current->next=first;
+// first = current，否则判断节点与前后节点的关系，即current->val与tmp->val和tmp->next->val的大小关系，
+// 如果存在其中，则current->next = tmp->next,tmp->next = current，
+// 最后一种情况就是循环到最后一个节点，tmp->next == nullptr，有之前的两个判断条件，此时一定有current->val>tmp->val
+// 整个题目的逻辑就是如此，需要两层while循环，第一层为待排序链表node，第二层为已经排序的链表node
 //
 
 #include <iostream>

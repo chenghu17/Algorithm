c485c5e4049e82652552e15d71539c741e99078a
CallmeMrhu <mrhu666@163.com>
2018-08-06 15:51:37 +0800

    commit

--- Leetcode/c_867_Transpose_Matrix.cpp
+++ Leetcode/c_867_Transpose_Matrix.cpp
@@ -3,8 +3,11 @@
 //
 // leetcode 867 transpose matrix
 //
+// 题目要求对给定的矩阵求转置
+// 思路比较简单，第i行第j列的值，变为第j行第i列的值
+// i行j列的矩阵的转置为j行i列
+// 对应位置赋值即可
 //
-
 
 #include <iostream>
 #include <vector>
@@ -12,12 +15,12 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> transpose(vector<vector<int>>& A) {
+    vector<vector<int>> transpose(vector<vector<int>> &A) {
         int row = A.size();
         int col = A[0].size();
-        vector<vector<int>> ts(col,vector<int>(row));
-        for(int i=0;i<row;++i){
-            for(int j=0;j<col;++j){
+        vector<vector<int>> ts(col, vector<int>(row));
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
                 ts[j][i] = A[i][j];
             }
         }
@@ -25,7 +28,7 @@ public:
     }
 };
 
-int main(){
+int main() {
     vector<vector<int>> A;
     vector<int> x;
     x.emplace_back(1);
@@ -39,10 +42,10 @@ int main(){
     A.emplace_back(x);
     Solution solution;
     vector<vector<int>> result = solution.transpose(A);
-    for(auto n:result){
-        for(auto m:n){
-            cout<<m<<" ";
+    for (auto n:result) {
+        for (auto m:n) {
+            cout << m << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }

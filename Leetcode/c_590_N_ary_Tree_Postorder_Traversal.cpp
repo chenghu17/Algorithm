c99eb7bf7409c623a343e658787f05ae36ee761f
CallmeMrhu <mrhu666@163.com>
2018-12-24 10:38:23 +0800

    commit

--- Leetcode/c_590_N_ary_Tree_Postorder_Traversal.cpp
+++ Leetcode/c_590_N_ary_Tree_Postorder_Traversal.cpp
@@ -3,6 +3,12 @@
 //
 // leetcode 590 n ary tree postorder traversal
 //
+// 题目要求对N叉树进行后序遍历
+//
+// 这里使用递归的方式进行遍历，类似于二叉树的后序遍历，只是在处理节点上，需要先循环访问节点的孩子节点，再访问当前节点。
+// 使用循环的方式进行实现同样需要借助栈(stack)来进行，这里不再重复。
+// 这里循环的方式和先序有一点不同，在于对节点进行访问后，每次访问孩子节点后，
+// 需要将节点的孩子节点置为nullptr（具体操作应该是将vector<Node*> children进行pop清空操作）
 //
 
 
@@ -44,7 +50,7 @@ public:
     }
 };
 
-int main(){
+int main() {
     vector<Node *> children1;
     Node e, f;
     e.val = 5;
